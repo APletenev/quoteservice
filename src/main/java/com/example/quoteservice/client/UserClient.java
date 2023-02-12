e37250a844001c6d7c0401a1c5d36aae259b5198
Andrey Pletenev <pletenev.andrey@yandex.ru>
2023-02-12 16:56:49 +0700

    Created client to communicate with userservice

---
+++ src/main/java/com/example/quoteservice/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.example.quoteservice.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
+
+@FeignClient(value = "user-api", url = "http://localhost:8080/api/user")
+public interface UserClient {
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}")
+    Optional getUserById(@PathVariable long id);
+}
+
